420a8960b3d3e3dc8cc7711a89170cb7752381bd
Emir Turkes <eturkes@bu.edu>
2018-10-11 17:03:42 -0400

    Add function to load data and check validity

--- pymice_analyzer/data.py
+++ pymice_analyzer/data.py
@@ -18,3 +18,41 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #    Emir Turkes can be contacted at eturkes@bu.edu
+
+import pymice as pm
+
+def load_data(*args, **kwargs):
+    """This function loads the data and checks its validity."""
+
+    dataFiles = [0 for x in range(len(args))]
+    for i in range(0, len(args)):
+        dataFiles[i] = args[i]
+
+    # Merge the data.
+    loaders = [pm.Loader(filename) for filename in dataFiles]
+    data = pm.Merger(*loaders)
+
+    print("Done loading data.")
+
+    for mouse in sorted(data.getGroup()):
+        print(mouse)
+
+    # Read in period of analysis from timeline.ini.
+    timeline = pm.Timeline('../timeline/hab1.ini')
+    start1, end1 = timeline.getTimeBounds(kwargs['phase1'])
+    start2, end2 = timeline.getTimeBounds(kwargs['phase2'])
+    print("%s:\t%s - %s" % (kwargs['phase1'], start1, end1))
+    print("%s:\t%s - %s" % (kwargs['phase2'], start2, end2))
+
+    # Check for any problems (indicated in the log) during the period of
+    # interest.
+    dataValidator = pm.DataValidator(pm.PresenceLogAnalyzer())
+    validatorReport = dataValidator(data)
+
+    noPresenceProblems = pm.FailureInspector('Presence')
+
+    if noPresenceProblems(validatorReport, (start1, end1)):
+        if noPresenceProblems(validatorReport, (start2, end2)):
+            print("Presences OK.")
+
+    return data, start1, end1, start2, end2
