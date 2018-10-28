57f1eacb542c66cb7b3cd482a3e700b4307d4d26
Emir Turkes <eturkes@bu.edu>
2018-10-27 22:49:26 -0400

    Add main arguments

--- pymice_analyzer/main.py
+++ pymice_analyzer/main.py
@@ -47,7 +47,7 @@ def parse_args():
         action="store",
         default=stored_args.get("data_directory"),
         widget="DirChooser",
-        help="Source directory containing Intellicage files",
+        help="Input directory containing Intellicage files",
     )
     parser.add_argument(
         "output_directory",
@@ -55,6 +55,90 @@ def parse_args():
         widget="DirChooser",
         default=stored_args.get("output_directory"),
         help="Output directory to save analysis",
+    )
+    parser.add_argument(
+        "project_name",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Name of your project",
+    )
+    parser.add_argument(
+        "paradigms",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Paradigms to use in the analysis",
+    )
+    parser.add_argument(
+        "start",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Start times and dates of the phases",
+    )
+    parser.add_argument(
+        "end",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="End times and dates of the phases",
+    )
+    parser.add_argument(
+        "excluded_groups",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Groups to exclude",
+    )
+    parser.add_argument(
+        "excluded_animals",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Animals to exclude",
+    )
+    parser.add_argument(
+        "comparisons",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="What comparisons to make",
+    )
+    parser.add_argument(
+        "error",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Way error should be measured",
+    )
+    parser.add_argument(
+        "normality",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Which normality tests to use",
+    )
+    parser.add_argument(
+        "variance",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Which variance tests to use",
+    )
+    parser.add_argument(
+        "tests",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Which statistical tests to use",
+    )
+    parser.add_argument(
+        "post_hoc",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Which post hoc tests to use",
+    )
+    parser.add_argument(
+        "plots",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Types of plots to make",
+    )
+    parser.add_argument(
+        "tables",
+        action="store",
+        default=stored_args.get("paradigms"),
+        help="Types of tables to make",
     )
     args = parser.parse_args()
     # Store the values of the arguments so that it is available on next run.
@@ -65,5 +149,5 @@ def parse_args():
 
 
 if __name__ == "__main__":
-    CONF = parse_args()
+    conf = parse_args()
     print("Done")
